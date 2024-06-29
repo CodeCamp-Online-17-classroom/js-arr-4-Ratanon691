88a99954c2784cb620ac06dd182d71f654ce32f8
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-29 13:23:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// write code here
+let arr = []
+let input
+
+while (true) {
+  input = prompt("Enter a number (or a non-number to stop):")
+  let num = Number(input)
+  if (!isNaN(num)) {
+    arr.push(num)
+  } else {
+    break
+  }
+}
+let sum = arr.reduce((acc, current) => acc + current, 0)
+
+console.log("Numbers entered:", arr)
+console.log("Sum of numbers:", sum)
+
